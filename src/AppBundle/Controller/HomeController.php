385f70ddaae6fdd6317ef02fadb9e29f66fa0a3b
pieterduchateau <pieterduchateau>
2018-07-09 19:45:54 +0000

    Basic calendar library added

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -2,9 +2,15 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Offerte;
+use DateTime;
+use Doctrine\Common\Collections\ArrayCollection;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Validator\Constraints\Date;
 
 class HomeController extends Controller
 {
@@ -13,7 +19,7 @@ class HomeController extends Controller
      */
     public function homepage($shop)
     {
-        return $this->render('default/index.html.twig',array(
+        return $this->render('default/index.html.twig', array(
             'shop' => $shop
         ));
     }
@@ -32,5 +38,50 @@ class HomeController extends Controller
     public function redirect_select_shop()
     {
         return $this->redirectToRoute('select_shop');
+    }
+
+    /**
+     * @Route("/{shop}/calendar", name="calendar")
+     */
+    public function calendar($shop)
+    {
+        return $this->render('default/calendar.html.twig', array(
+            'shop' => $shop
+        ));
+    }
+
+    /**
+     * @Route("/getEvents", name="getEvents")
+     */
+    public function getEvents(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $first = $request->request->get('start');
+        $last = $request->request->get('end');
+        $shop = $request->request->get('shop');
+
+        $repository = $this->getDoctrine()
+            ->getRepository(Offerte::class);
+        $query = $repository->createQueryBuilder('o')
+            ->where('o.delivery_date >= :firstday')
+            ->andwhere('o.delivery_date <= :endday')
+            ->setParameter('firstday', $first)
+            ->setParameter('endday', $last)
+            ->getQuery();
+        $events = $query->getResult();
+
+        $eventArray = [];
+        foreach ($events as $event) {
+            $h1 = array(
+                'id' => $event->getId(),
+                'title' => $event->getTitel(),
+                'start' => $event->getDeliveryDate()->format('Y-m-d'),
+                'shop' => $shop
+            );
+
+            array_push($eventArray, $h1);
+        }
+
+        return new JsonResponse($eventArray);
     }
 }
